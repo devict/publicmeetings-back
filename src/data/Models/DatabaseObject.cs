94506b74d1edd2701350fcc74b2a4cb7f349d6d4
Rexy Que <rexy.que@gmail.com>
2019-11-08 13:09:49 -0600

    Start work on models

---
+++ src/data/Models/DatabaseObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DevIct.PublicMeetings.Back.Data.Models
+{
+    /// <summary>
+    /// The base class for all models in the database.
+    /// </summary>
+    public abstract class DatabaseObject
+    {
+        protected readonly Guid _id;
+        
+        /// <summary>
+        /// Creates a new <see cref="DatabaseObject"/>.
+        /// </summary>
+        public DatabaseObject()
+        {
+            _id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="DatabaseObject"/> with <paramref name="id"/>
+        /// as its unique ID.
+        /// </summary>
+        public DatabaseObject(Guid id)
+        {
+            _id = id;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Guid"/> assigned to the <see cref="DatabaseObject"/>.
+        /// </summary>
+        public virtual Guid Id => _id;
+    }
+}
---
+++ src/data/Models/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DevIct.PublicMeetings.Back.Data.Models
+{
+    public class User : DatabaseObject
+    {
+        public User(
+            Guid id,
+            string first,
+            string last,
+            string username,
+            UserRole role,
+            string? email = null) : base(id)
+        {
+            FirstName = first;
+            LastName = last;
+            Username = username;
+            Role = role;
+            Email = email;
+        }
+
+        public User(
+            string first,
+            string last,
+            string username,
+            UserRole role,
+            string? email = null) : this(
+                Guid.NewGuid(),
+                first,
+                last,
+                username,
+                role,
+                email)
+        {
+
+        }
+
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Username { get; set; }
+        public virtual UserRole Role { get; set; }
+        public virtual string? Email { get; set; }
+    }
+
+    public enum UserRole
+    {
+        User = 0,
+        Administrator = 100
+    }
+}
